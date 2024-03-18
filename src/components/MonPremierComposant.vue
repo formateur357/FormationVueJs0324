37b8ae2b03b20d54357931263477b90e9b1ec7fd
moguy93 <guymorgan@live.fr>
2024-03-18 10:18:53 +0100

    add first component

---
+++ src/components/MonPremierComposant.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mon-premier-composant">
+    Bonjour, c'est mon premier composant Vue !
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonPremierComposant'
+}
+</script>
+
+<style>
+.mon-premier-composant {
+  font-size: 20px;
+  color: blue;
+}
+</style>
